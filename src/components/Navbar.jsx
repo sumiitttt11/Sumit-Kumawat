687816968bf504ee830523f1cc9c88ac5dda7880
sumiitttt11 <kumawatsumit1108@gmail.com>
2024-11-15 21:01:27 +0530

    Updated Navbar.js component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = () => {
         <img className="mx-2 w-10" src={logo} alt="logo" />
       </div>
       <div className="m-8 flex items-center justify-center gap-4 text-2xl">
-        <a href="https://www.linkedin.com/in/sumit-kumawat-5b5a01287/" target="_blank" rel="noopener noreferrer">
+        <a href="https://www.linkedin.com/in/sumiitttt11/" target="_blank" rel="noopener noreferrer">
           <FaLinkedin />
         </a>
         <a href="https://github.com/sumiitttt11" target="_blank" rel="noopener noreferrer">
